00bbc810e2544d26c115e8177497134dd3000874
comfortyelfaare <comfortyel405@gmail.com>
2022-08-23 10:35:20 -0700

    commit two

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from '../components/Header'
 
 function AboutScreen() {
   return (
-    <div>AboutScreen</div>
+    <div>
+    <Header/>
+    <p>the application is all about countries,
+      Ghana, the capital city is Wa,the
+      currency is in Ghana cied and the colors is red,gold, green 
+      In horizontal stripes with five point black stark at the center of the  flag </p>
+    </div>
   )
 }
 
